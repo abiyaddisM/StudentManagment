048334c43a0576b6089e38521c18ced993be80ec
I-hates-debugging <dagmfek@gmail.com>
2023-09-13 23:55:07 +0300

    backend

--- src/Pages/ManagePage/ManagePanel.java
+++ src/Pages/ManagePage/ManagePanel.java
@@ -7,12 +7,18 @@ import BackEnd.RowInfoHolder;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ManagePanel extends JPanel {
     static ArrayList<RowInfoHolder> studentInfoHolders=new ArrayList<>();
     static ArrayList<RowInfoHolder> teacherInfoHolders=new ArrayList<>();
     static ArrayList<RowInfoHolder> staffInfoHolders=new ArrayList<>();
+
+    static String jdbcUrl = "jdbc:mysql://localhost:3306/school_management";
+    static String jdbcUsername = "root";
+    static String jdbcPassword = "password";
 
 
     static  {
@@ -86,9 +92,47 @@ public class ManagePanel extends JPanel {
     static void getStudentInfoFromDatabase(){
         //Connect the sql here and put the data of students here.
 
-        for (int i = 0; i < 10; i++) {
-            studentInfoHolders.add(new RowInfoHolder("0Q"+i,"Abiy","Addis","1997","April","2","Male","Students","abiy@gmail.com","0980633711"));
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
+
+            String selectSql = "SELECT student.first_name, student.last_name, student.date_of_birth, student.display_id,\n" +
+                    "                    student.gender, student_email.email, student_phone_number.phone_number, department.department_name\n" +
+                    "                    FROM student\n" +
+                    "                    LEFT JOIN student_email ON student.student_id = student_email.student_id\n" +
+                    "                    LEFT JOIN student_phone_number ON student.student_id = student_phone_number.student_id\n" +
+                    "                    LEFT JOIN department ON student.department_id = department.department_id;";
+
+            PreparedStatement selectStatement = connection.prepareStatement(selectSql);
+            ResultSet resultSet = selectStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String displayID = resultSet.getString("display_id");
+                String gender = resultSet.getString("gender");
+                String emailAddress = resultSet.getString("email");
+                String phoneNumber = resultSet.getString("phone_number");
+                String departmentName = resultSet.getString("department_name");
+
+                Date sqlDate = resultSet.getDate("date_of_birth");
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(sqlDate);
+
+                String year = Integer.toString(calendar.get(Calendar.YEAR));
+                String month = Integer.toString(calendar.get(Calendar.MONTH) + 1); // Adding 1 because months are zero-based
+                String day = Integer.toString(calendar.get(Calendar.DAY_OF_MONTH));
+
+
+                studentInfoHolders.add(new RowInfoHolder(displayID,firstName,lastName,year,month,day,gender,departmentName,emailAddress,phoneNumber));
+            }
+            resultSet.close();
+            selectStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
 
     }
     static void getTeacherInfoFromDatabase(){
