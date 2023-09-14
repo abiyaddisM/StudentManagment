be7d0c110a99a88a0539d5506d8f014fffd6f91a
abiyaddisM <130771857+abiyaddisM@users.noreply.github.com>
2023-09-15 01:55:04 +0300

    frontend for edit

--- src/Pages/ManagePage/Register/EditStudentPanel.java
+++ src/Pages/ManagePage/Register/EditStudentPanel.java
@@ -4,6 +4,7 @@ import BackEnd.AddInfo;
 import BackEnd.DeleteInfo;
 import BackEnd.EditInfo;
 import BackEnd.RowInfoHolder;
+import Pages.ManagePage.List.StudentListPanel;
 import Pages.ManagePage.ManagePanel;
 
 import javax.swing.*;
@@ -54,6 +55,7 @@ public class EditStudentPanel extends ManageRegisterPanel {
                     RowInfoHolder infoHolder=new RowInfoHolder(studentIdLabel.getText(),firstNameTextBox.getString(),lastNameTextBox.getString(),yearComboBox.getText(), "February",dayComboBox.getText(),genderComboBox.getText(),studentBatchComboBox.getText(),emailTextBox.getString(),phoneNoTextBox.getString());
                     EditInfo editInfo = new EditInfo(infoHolder);
                     editInfo.updateStudent();
+                    ManagePanel.studentListPanel.updateInfo(index,infoHolder);
                     ManagePanel.backToOptionPanel();
                     clearAll();
                 }
@@ -63,52 +65,6 @@ public class EditStudentPanel extends ManageRegisterPanel {
         saveButton.label.addMouseListener(action);
     }
 
-    public static String convertMonth(int monthNumber) {
-        String monthName;
-
-        switch (monthNumber) {
-            case 1:
-                monthName = "January";
-                break;
-            case 2:
-                monthName = "February";
-                break;
-            case 3:
-                monthName = "March";
-                break;
-            case 4:
-                monthName = "April";
-                break;
-            case 5:
-                monthName = "May";
-                break;
-            case 6:
-                monthName = "June";
-                break;
-            case 7:
-                monthName = "July";
-                break;
-            case 8:
-                monthName = "August";
-                break;
-            case 9:
-                monthName = "September";
-                break;
-            case 10:
-                monthName = "October";
-                break;
-            case 11:
-                monthName = "November";
-                break;
-            case 12:
-                monthName = "December";
-                break;
-            default:
-                monthName = "Invalid month";
-        }
-
-        return monthName;
-    }
 
 
 
